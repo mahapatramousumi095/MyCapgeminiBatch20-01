cbadf165e802b97bd15f34277fd7db0c8690ee52
mahapatramousumi095 <mahapatramousumi095@gmail.com>
2022-06-01 17:06:31 +0530

    This is my second comit

--- TestGitProject-01/src/com/test/MyTest.java
+++ TestGitProject-01/src/com/test/MyTest.java
@@ -5,6 +5,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my test class");
+		System.out.println("This is update checking");
 
 	}
 
